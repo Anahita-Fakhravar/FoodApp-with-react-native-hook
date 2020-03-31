d378c011dd3d3264c56890588a7e0f93aef4252f
ASUS <anahita_fakhravar@yahoo.com>
2020-03-31 11:56:52 +0430

    Define new pages in index.js

--- index.js
+++ index.js
@@ -4,15 +4,34 @@
 
 import {Navigation} from 'react-native-navigation';
 import Login from './src/screens/Login';
+import Main from './src/screens/Main';
 
-Navigation.registerComponent(`Login`, () => Login);
+Navigation.registerComponent(`myLogin`, () => Login);
+Navigation.registerComponent(`myMain`, () => Main);
 
 Navigation.events().registerAppLaunchedListener(() => {
     Navigation.setRoot({
         root: {
-            component: {
-                name: 'Login',
-            },
+            stack:
+                {
+                    id: 'FoodApp',
+                    children: [
+                        {
+                            component: {
+                                id: 'myLoginId',
+                                name: 'myLogin',
+                                options: {
+                                    topBar: {
+                                        visible: false,
+                                        height: 0,
+                                    },
+                                }
+                            }
+                        },
+
+                    ],
+
+                },
         },
     });
 });
